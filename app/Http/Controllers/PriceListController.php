d484833d910b23d2abef24bc7c77d144e2d4b4db
verHsn <md.hassan@vertitect.com>
2025-01-23 13:21:09 +0530

    price list upload functionality

--- app/Http/Controllers/PriceListController.php
+++ app/Http/Controllers/PriceListController.php
@@ -11,16 +11,17 @@ class PriceListController extends Controller
     public function DailyPriceList()
     {
         $data = DB::table('daily_price_list')->get();
+        // dd($data);
         return view('price-list.index',compact('data'));
     }
 
     public function uploadCsv(Request $request)
     {
         $request->validate([
-            'csv_upload_t1' => 'required|mimes:csv,txt|max:2048',
+            'daily_price_list' => 'required|mimes:csv,txt|max:2048',
         ]);
 
-        $file = $request->file('csv_upload_t1');
+        $file = $request->file('daily_price_list');
         $filePath = $file->getRealPath();
 
         // Read CSV file
@@ -34,17 +35,17 @@ class PriceListController extends Controller
             fclose($handle);
 
             foreach ($rows as $row) {
-                [$stateName, $districtName, $cityName, $categoryName, $productName, $price] = $row;
+                [$stateName, $districtName, $pin_code, $categoryName, $productName, $price] = $row;
 
                 // Find IDs based on names
                 $stateId = DB::table('states')->where('name', $stateName)->value('id');
                 $districtId = DB::table('districts')->where('name', $districtName)->value('id');
-                $cityId = DB::table('citys')->where('name', $cityName)->value('id');
+                $pincodeId = DB::table('pincodes')->where('pincode', $pin_code)->value('id');
                 $categoryId = DB::table('product_categories')->where('category_name', $categoryName)->value('id');
                 $productId = DB::table('products')->where('item', $productName)->value('id');
 
                 // Skip if any ID is not found
-                if (!$stateId || !$districtId || !$cityId || !$categoryId || !$productId) {
+                if (!$stateId || !$districtId || !$pincodeId || !$categoryId || !$productId) {
                     return redirect()->back()->with('error', 'Some records have invalid names.');
                 }
 
@@ -53,7 +54,7 @@ class PriceListController extends Controller
                     [
                         'state_id' => $stateId,
                         'district_id' => $districtId,
-                        'city_id' => $cityId,
+                        'pin_id' => $pincodeId,
                         'category_id' => $categoryId,
                         'product_id' => $productId,
                     ],
